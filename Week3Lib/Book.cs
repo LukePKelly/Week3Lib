cdbbd9bab4bdb8690501da86323b3db1e3346dc4
LukePKelly <Kelly-L108@ulster.ac.uk>
2025-10-06 16:35:24 +0100

    Access Modifier

---
+++ Week3Lib/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Lib
+{
+    public class Book
+    {
+        string Title;
+        string Author;
+        string ISBN;
+
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+    }
+ }
+
+
--- Week3Lib/Program.cs
+++ Week3Lib/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Week3Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Creates a new instance of the Book class, object name differs from the class name
+        Book book = new Book("C# For beginners", "Bill Gates", "12345");
+        //Output book information to the console
+        book.DisplayInfo();
+    }
+}
